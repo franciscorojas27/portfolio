d9856ff0d0ce20d15c26aea39ec8ec2c3e38fe3f
Francisco Rojas <67331645+franciscorojas27@users.noreply.github.com>
2025-07-24 22:31:15 -0400

    adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
@@ -10,7 +13,7 @@ export default defineConfig({
     schema: {
       EMAIL: envField.string({ context: "client", access: "public" }),
       DRIVE_CV: envField.string({ context: "client", access: "public" }),
-      EMAIL_API: envField.string({context: 'server',access:'secret'}),
+      EMAIL_API: envField.string({ context: "server", access: "secret" }),
       LINKEDIN_LINK: envField.string({ context: "client", access: "public" }),
       GITHUB_LINK: envField.string({ context: "client", access: "public" }),
       GMAIL_PASS: envField.string({ context: "server", access: "secret" }),
