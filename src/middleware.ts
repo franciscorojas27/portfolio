018c1215c8a6ebb5c29e0f6676ee9aeb3f3f667a
Francisco Rojas <67331645+franciscorojas27@users.noreply.github.com>
2025-07-25 14:56:01 -0400

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+export async function onRequest(context: any) {
+  const response = await context.next();
+
+  const newHeaders = new Headers(response.headers);
+  newHeaders.set(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'; require-trusted-types-for 'script'"
+  );
+  newHeaders.set(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains; preload"
+  );
+  newHeaders.set("X-Frame-Options", "DENY");
+  newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+  newHeaders.set("X-Content-Type-Options", "nosniff");
+  newHeaders.set("Referrer-Policy", "strict-origin-when-cross-origin");
+  newHeaders.set(
+    "Permissions-Policy",
+    "geolocation=(), microphone=(), camera=()"
+  );
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders,
+  });
+}
